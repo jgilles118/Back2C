f12943b6a89794e8a1de22269471f79942a55042
J Gilles <jgilles123@gmail.com>
2023-08-27 16:53:45 -0400

    Symbolic Const are done

---
+++ symbolic_const.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Use #define declare a symbolic name or constant
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
+
+int main(){
+
+    int fahr;
+
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP){
+        
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+     }
+
+    return 0;
+}
